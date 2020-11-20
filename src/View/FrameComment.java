b3c492a955874b78c653b35bf6814fbfc43988f5
HansNotFound <HansNotFound@DESKTOP-O2I3T19>
2020-11-20 16:20:13 +0700

    Manambah Friend Req Mengganti pemanggilan idPost ke Comment

--- src/View/FrameComment.java
+++ src/View/FrameComment.java
@@ -26,52 +26,54 @@ import javax.swing.WindowConstants;
  *
  * @author HansNotFound
  */
-public class FrameComment extends JFrame implements ActionListener{
+public class FrameComment extends JFrame implements ActionListener {
+
     JFrame frame_Comment;
     JTextField comments;
     JButton submit;
     User containUser;
-    String nicknameUser,isiComment;
-    int IdPost,counter;
+    String nicknameUser, isiComment;
+    int IdPost, counter;
     Comment comment = new Comment();
-    public FrameComment(User users,int idPost,int counter_post){
+
+    public FrameComment(User users, int idPost, int counter_post) {
 
         containUser = users;
         nicknameUser = users.getNickname();
         IdPost = idPost;
         counter = counter_post;
         ArrayList<Comment> listComment = ControllerComment.getListCommentByIDPost(IdPost);
-        DefaultListModel<String> l1 = new DefaultListModel<>(); 
-        
+        DefaultListModel<String> l1 = new DefaultListModel<>();
+
         frame_Comment = new JFrame("Comment");
         frame_Comment.setSize(300, 500);
         frame_Comment.setLocationRelativeTo(null);
-        frame_Comment.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); 
-        
+        frame_Comment.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
         comments = new JTextField();
         comments.setBounds(30, 50, 100, 30);
-        
+
         submit = new JButton("PostComment");
         submit.setBounds(100, 80, 100, 30);
         submit.addActionListener(this);
-        
+
         //int width = 20;
         //String strLabel = "label_";
         JLabel label = new JLabel();
-        for(int i = 0; i < listComment.size() ; i++){
+        for (int i = 0; i < listComment.size(); i++) {
             l1.addElement(listComment.get(i).getIsiComment());
         }
-        
+
         JList<String> list = new JList<>(l1);
-        list.setBounds(100,100, 90,90); 
+        list.setBounds(100, 100, 90, 90);
         list.setEnabled(false);
-        
+
         UIManager.put("Label.disabledForeground", Color.black);
-        
+
         frame_Comment.add(comments);
         frame_Comment.add(submit);
         frame_Comment.add(list);
-        
+
         frame_Comment.setLayout(null);
         frame_Comment.setVisible(true);
     }
@@ -79,18 +81,25 @@ public class FrameComment extends JFrame implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent ae) {
         String command = ae.getActionCommand();
-        switch(command){
+        switch (command) {
             case "PostComment":
                 isiComment = comments.getText();
-                comment.setIsiComment(isiComment);
-                comment.setNicknameComment(nicknameUser);
-                boolean insertComment = ControllerComment.insertNewComments(comment,IdPost);
-                if(insertComment){
-                    JOptionPane.showMessageDialog(null, "Comment berhasil di post");
-                    TimeLine timeLine = new TimeLine(containUser, counter);
-                    frame_Comment.setVisible(false);
-                }else{
-                    JOptionPane.showMessageDialog(null, "Comment gagal di post");                
+                if (isiComment.equals("")) {
+                    JOptionPane.showMessageDialog(null, "Comment masih kosong!!", "Error", JOptionPane.ERROR_MESSAGE);
+                    break;
+                } else {
+                    comment.setIsiComment(isiComment);
+                    comment.setNicknameComment(nicknameUser);
+                    boolean insertComment = ControllerComment.insertNewComments(comment, IdPost);
+                    if (insertComment) {
+                        JOptionPane.showMessageDialog(null, "Comment berhasil di post");
+                        TimeLine timeLine = new TimeLine(containUser, counter);
+                        frame_Comment.setVisible(false);
+                        break;
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Comment gagal di post");
+                        break;
+                    }
                 }
         }
     }
