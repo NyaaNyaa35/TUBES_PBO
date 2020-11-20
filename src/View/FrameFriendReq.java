92a1451f2351c8614a88b0511021d56cc4c6cfdf
HansNotFound <HansNotFound@DESKTOP-O2I3T19>
2020-11-20 14:11:37 +0700

    Manambah FriendReq Frame doang

---
+++ src/View/FrameFriendReq.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JList;
+
+/**
+ *
+ * @author HansNotFound
+ */
+public class FrameFriendReq extends JFrame implements ActionListener{
+    JFrame frame;
+    JList list_request;
+    public FrameFriendReq(){
+        
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+}
